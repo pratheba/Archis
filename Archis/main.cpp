d5989d0bade5c67c995715c1edf7ee0cb690cbee
Pratheba <pratheba@gmail.com>
2015-09-15 01:59:26 +0400

    Initial Commit

---
+++ Archis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Archis
+//
+//  Created by Pratheba Selvaraju on 9/15/15.
+//  Copyright (c) 2015 Pratheba Selvaraju. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
